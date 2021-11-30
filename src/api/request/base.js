7e58b552f0444b48a364467bd45daa32c1a55bd3
lpc528150 <1843449425@qq.com>
2021-11-30 17:55:01 +0800

    11-30

--- src/api/request/base.js
+++ src/api/request/base.js
@@ -2,12 +2,7 @@
  * 接口域名的管理
  */
 const base = {    
-<<<<<<< HEAD
-    sq: 'http://172.20.174.245:11119', 
-=======
-    // sq: 'http://rolin.icu:11119', 
-	sq: 'http://172.20.174.245:11119',
->>>>>>> origin/lch
+    sq: 'http://rolin.icu:11119', 
 	test:'http://test.30202.co:11119'
 }
 
